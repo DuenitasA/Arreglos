2aa360c017aa18efd1208eb93199b7b49a999849
Alejandro Duenas <alejandro.duenas@udla.edu.ec>
2023-11-20 16:42:36 -0500

    segunda ideaa

--- candidato.c
+++ candidato.c
@@ -5,34 +5,21 @@
 int main(void)
 {
 
-    srand(time(NULL));
-
-    int votos[];
-    int uno = 0, dos = 0, tres = 0, cuatro = 0, cinco = 0;
-    for (int i = 0; i < 20; i++)
+    int votos[5];
+    int voto;
+    do
     {
-        votos[i] = rand() % 5 + 1;
-        if (votos[i] == 1)
+        while (voto > 5 && voto < 0)
         {
-            uno++;
+        printf("Ingrese su voto, cuando sea la ultima persona ingrese 0.\n");
+        scanf("%d", &voto);
         }
-        else if (votos[i] == 2)
-        {
-            dos++;
-        }
-        else if (votos[i] == 3)
-        {
-            tres++;
-        }
-        else if (votos[i] == 4)
-        {
-            cuatro++;
-        }
-        else if (votos[i]== 5)
-        {
-            cinco++;
-        }
-    }
+        
+
+    } while (voto != 0);
+    
+
+
 
     for (int i = 0; i < 20; i++)
     {
@@ -40,15 +27,5 @@ int main(void)
     }
     printf("\n");
 
-    printf("El candidadato 1 tiene %d votos.\n", uno);
-    printf("El candidadato 2 tiene %d votos.\n", dos);
-    printf("El candidadato 3 tiene %d votos.\n", tres);
-    printf("El candidadato 4 tiene %d votos.\n", cuatro);
-    printf("El candidadato 5 tiene %d votos.\n", cinco);
-
-
-    
-
-    fflush(stdin);
     return 0;
 }
